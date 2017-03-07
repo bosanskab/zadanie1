30eef4d0d82cd12385b334a9c89c80a49893c07e
DESKTOP-CR62HTI\Barborka <bosanska.bar@gmail.com>
2017-03-07 17:53:14 +0100

    no message

--- zadanie1-výmena.cpp
+++ zadanie1-výmena.cpp
@@ -1,25 +1,26 @@
 #include <stdio.h>
 
 short int swap_endianness(short int *x){
-	
-	short int *y;
+
 	char *m,*n,o;
 	
-	y=x;
-	m=(char*)y;
-	n=(char*)y+1;
+	m=(char*)x;
+	n=(char*)x+1;
 	
 	o=*m;
 	*m=*n;
 	*n=o;
 		
-	return *y;
+	return *x;
 }
 
 main(){
 	short int x;
 	
+	printf("zadaj x:",x);
 	scanf("%d",&x);
 	swap_endianness(&x);
-	printf("\n%d",x);
+	printf("\n vymenene:%d",x);
+	
+	return 0;
 }
